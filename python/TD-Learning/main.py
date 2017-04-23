5e94c952ab6402b77f4f5a0fe29835af03170491
Kaeronor <nicolas.talabot@epfl.ch>
2017-04-23 12:30:55 +0200

    Start of TD-learning

---
+++ python/TD-Learning/main.py
@@ -0,0 +1,8 @@
+import nim
+
+# RL Parameters
+stepSize = 0.1 	# Learning rate
+discount = 1	# No discounting
+lambdaReturn = 0.9 # For the eligibility traces
+epsilon  = 0.1	# Parameter of the e-greedy policy
+
