eb2dccd89a27931dc18e35fb704f2d9589208bbc
Kaeronor <nicolas.talabot@epfl.ch>
2017-04-23 14:59:18 +0200

    Added Nim parameters

--- python/TD-Learning/main.py
+++ python/TD-Learning/main.py
@@ -6,3 +6,7 @@ discount = 1	# No discounting
 lambdaReturn = 0.9 # For the eligibility traces
 epsilon  = 0.1	# Parameter of the e-greedy policy
 
+# Nim Parameters
+heapMax = 4		# Maximum number of heaps
+itemMax = 5		# Maximum number of items per heap
+
